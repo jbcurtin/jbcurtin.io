dc4fdf74ad058e28a842832034a87e9a48eb1192
Joseph Curtin <42@jbcurtin.io>
2020-04-15 15:58:25 -0400

    Debug Build

--- build-tools/builder/build-website.py
+++ build-tools/builder/build-website.py
@@ -113,6 +113,7 @@ def find_notebooks() -> types.GeneratorType:
 
 
 def build_index_page(notebooks: typing.List[Notebook]) -> None:
+    print(os.getcwd())
     for item in os.listdir(TEMPLATE_DIR):
         print(item)
     environment: Environment = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR), undefined=jinja2.StrictUndefined)
