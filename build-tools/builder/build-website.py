6aad7522ed1d98db8a8a3808dc99d7e802551183
Joe Curtin <42@jbcurtin.io>
2020-11-27 05:29:14 +0900

    debug GH Actions

--- build-tools/builder/build-website.py
+++ build-tools/builder/build-website.py
@@ -86,6 +86,7 @@ def _enhance_notebook_metadata(notebook: Notebook) -> None:
         notebook.metadata['publish_date'] = first_commit.authored_datetime
         notebook.metadata['updated_date'] = last_commit.authored_datetime
 
+    print(GIT_REPO_ROOT)
     for comm in commits:
         print(comm.hexsha)
     print(notebook.metadata['publish_date'])
